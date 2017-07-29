2bd4203da91e4ec73debab30596d5e5c899959b0
Carlos <cverdin@cpp.edu>
2017-07-29 15:40:11 -0700

    wireframes included

---
+++ AutoWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoWebsite.MvcApplication" Language="C#" %>
---
+++ AutoWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoWebsite.Startup))]
+namespace AutoWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
