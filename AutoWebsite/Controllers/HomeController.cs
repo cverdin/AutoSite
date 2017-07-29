8a1e22a6f502de7bdc2ec5145af64324788f6b1d
Carlos <cverdin@cpp.edu>
2017-07-29 15:57:21 -0700

    update

--- AutoWebsite/Controllers/HomeController.cs
+++ AutoWebsite/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace AutoWebsite.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Yo = "ABOUT THE PG";
 
             return View();
         }
